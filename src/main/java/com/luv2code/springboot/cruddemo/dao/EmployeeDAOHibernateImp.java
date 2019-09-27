f25acac9037ca4528269e60d71bd1fe640abbed5
Ravi Shankar Jha <ravishankarjha163@gmail.com>
2019-09-27 23:56:50 +0530

    added comment

--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOHibernateImp.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOHibernateImp.java
@@ -14,6 +14,7 @@ import com.luv2code.springboot.cruddemo.entity.Employee;
 @Repository
 public class EmployeeDAOHibernateImp implements EmployeeDAO {
 	
+	//making changes  to push 
 	private EntityManager theEntityManager;
 	@Autowired
 	public EmployeeDAOHibernateImp(EntityManager theEntityManager){
